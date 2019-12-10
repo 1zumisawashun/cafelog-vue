4fdbac2303d9c9e749d5aced8a18f5cefb127c4f
izumisawashun <43409339+izumisawashun@users.noreply.github.com>
2019-12-10 16:54:08 +0900

    レスポンシブ対応

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -10,7 +10,7 @@ class ShopsController < ApplicationController
     @area = Shop.where(area: "渋谷").limit(8)
     @tags =Shop.tagged_with(["wi-fi"]).limit(8)
     @score= Comment.average(:score)
-    @random= Shop.all
+    @images=Image.all
 
 
     @hutakotamagawa=Shop.where(area:"二子玉川")
