0c168f7e20c657d3c4ad9b42eae981cc288d16a2
izumisawashun <43409339+izumisawashun@users.noreply.github.com>
2019-12-13 11:22:27 +0900

    CSS

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -27,7 +27,7 @@ $(function() {
 		centerPadding: '60px', //前後のパディング
 		autoplay: true, //オートプレイ
 		autoplaySpeed: 2000, //オートプレイの切り替わり時間
-		slidesToShow: 3,
+		slidesToShow: 5,
 		responsive: [{
 			breakpoint: 768,
 			settings: {
